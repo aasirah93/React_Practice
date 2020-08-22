ba03fbc6a2c5109069e18ea9c67022b5590d959f
aasirah93 <62110929+aasirah93@users.noreply.github.com>
2020-08-22 22:52:40 +0100

    Created header component

---
+++ practice/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './components/Header'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
