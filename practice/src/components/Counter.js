a1349994f83f19d50d9aa185baed844bad2071a5
aasirah93 <62110929+aasirah93@users.noreply.github.com>
2020-08-22 23:35:08 +0100

    can setState

---
+++ practice/src/components/Counter.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+function Counter() {
+    const [count, setCount] = useState(0)
+    return (
+        <div>
+           <h1>
+             {count}
+           </h1> 
+           <button onClick={() => setCount(count + 1)}>
+             Plus
+           </button>
+           <button onClick={() => setCount(count - 1)}>
+             Minus
+           </button>
+        </div>
+    )
+}
+
+export default Counter
