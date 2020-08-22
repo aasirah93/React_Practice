9e509b071236694399d4af080e7176aac61ac425
aasirah93 <62110929+aasirah93@users.noreply.github.com>
2020-08-22 23:08:24 +0100

    props can be passed down as arguments

--- practice/src/components/Header.js
+++ practice/src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-function Header() {
+function Header(props) {
     return (
+        <div>
           <h1>
-            Hello there!
+            Hello there {props.name}
           </h1>
+          </div>
     );
 }
 
